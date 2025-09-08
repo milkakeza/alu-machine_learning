f1208dc75c584eca74f2f1525dac0244318d516b
milkakeza <milkakeza9@gmail.com>
2025-09-08 22:17:29 +0200

    Change of Scale

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module plots the exponential decay of C-14 as a line graph.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 28651, 5730)
+r = np.log(0.5)
+t = 5730
+y = np.exp((r / t) * x)
+
+plt.plot(x, y, 'b-')
+plt.xlabel("Time (years)")
+plt.ylabel("Fraction Remaining")
+plt.title("Exponential Decay of C-14")
+plt.yscale("log")
+plt.xlim(0, 28650)
+plt.savefig("decay_plot.png")
