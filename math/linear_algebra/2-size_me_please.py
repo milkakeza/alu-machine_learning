8f7c521393f7f96b83c78d41e0da8b9bb3a64102
milkakeza <milkakeza9@gmail.com>
2025-09-14 21:22:41 +0200

    Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+# Function for calculating shape of matrix
     shape = []
-    while isinstance(matrix,list):
+    while isinstance(matrix, list):
         shape.append(len(matrix))
         matrix = matrix[0]
     return shape
