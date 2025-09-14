4e557d4e8fe13ffff24a5e3d87e4e9ab7c3f6ab0
milkakeza <milkakeza9@gmail.com>
2025-09-14 21:42:44 +0200

    Across the planes

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+"""
+    Function adds two 2D matrices element-wise, and 
+    returns the list of lists or None: Element-wise sum if shapes match, else None.
+    """
+
+
 def add_matrices2D(mat1, mat2):
+    """
+    Adds two 2D matrices element-wise, and 
+    returns the list of lists or None: Element-wise sum if shapes match, else None.
+    """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
