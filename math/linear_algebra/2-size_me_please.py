9c69a2c2f0a42058d6d86330ad03673e98e12590
milkakeza <milkakeza9@gmail.com>
2025-09-14 21:26:46 +0200

    Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+This module contains the function matrix_shape which calculates
+the shape of matrix and returns it as a list of integers.
+"""
 
 def matrix_shape(matrix):
     # Function for calculating shape of matrix
