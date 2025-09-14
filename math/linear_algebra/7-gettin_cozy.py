75b8ea4ed0e38e43e75519ae765a5ff15e5a7746
milkakeza <milkakeza9@gmail.com>
2025-09-14 20:09:33 +0200

    Gettin' Cozy

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def cat_matrices2D(mat1, mat2, axis=0):
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        return [row[:] for row in mat1] + [row[:] for row in mat2]
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        return [row1[:] + row2[:] for row1, row2 in zip(mat1, mat2)]
+    else:
+        return None
