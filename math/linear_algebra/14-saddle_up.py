d63a3093db2946efa94625f5fef18b1e5f9d834e
milkakeza <milkakeza9@gmail.com>
2025-09-14 22:29:34 +0200

    Saddle up

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""
+Function performs matrix multiplication between two NumPy arrays.
+"""
 import numpy as np
+
+
 def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication between two NumPy arrays.
+    """
     return np.matmul(mat1, mat2)
