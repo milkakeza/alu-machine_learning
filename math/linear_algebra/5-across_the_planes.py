43c1d9cd05d22884f4555cc04be60cf7bab7392b
milkakeza <milkakeza9@gmail.com>
2025-09-14 21:48:28 +0200

    Across the planes

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -14,4 +14,5 @@ else None.
     """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    return [[a + b for a, b in zip(row1, row2)]
+            for row1, row2 in zip(mat1, mat2)]
