665a32823b4e3bc0e6d455defb827b06e3592899
milkakeza <milkakeza9@gmail.com>
2025-09-14 21:40:54 +0200

    Line up

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
+"""
+    Function Adds two arrays element-wise.
+    """
+
+
 def add_arrays(arr1, arr2):
+    """
+    Adds two arrays element-wise.
+
+    """
     if len(arr1) != len(arr2):
         return None
     return [a + b for a, b in zip(arr1, arr2)]
