d8a91510ee6e4851139b400fa305f7e20a604b25
milkakeza <milkakeza9@gmail.com>
2025-09-14 21:55:12 +0200

    I'll use my scale

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+"""
+Function returns the shape of a NumPy ndarray.
+"""
+
+
 def np_shape(matrix):
+    """
+    Returns the shape of a NumPy ndarray.
+    """
     return matrix.shape
